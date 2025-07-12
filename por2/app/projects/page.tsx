b2ddcd252f154f33a4256f3aa26158222e66c244
Logan Bjork <loganbjork@ufl.edu>
2025-07-12 18:58:56 -0400

    projects done; resume reuploaded

--- por2/app/projects/page.tsx
+++ por2/app/projects/page.tsx
@@ -7,6 +7,20 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 
 export default function projects() {
   return (
@@ -31,13 +45,47 @@ export default function projects() {
                     <CardTitle className="text-3xl">EnviroPact</CardTitle>
                   </CardHeader>
                   <CardContent className="flex flex-col flex-grow gap-4 w-full items-center">
-                      <Image className="rounded-md" src="/proj/envr.png" width={600} height={200} alt="A screenshot from ENVIROPACT's homepage"/>
-                      BLURB FIXME
+                      <Image className="rounded-md" src="/proj/env.png" width={600} height={200} alt="A screenshot from ENVIROPACT's homepage"/>
+                      {/* BLURB FIXME */}
                   </CardContent>
                   <CardFooter>
-                    <div className="flex flex-row gap-4 w-full justify-between">
-                      <p>TECH SHIT HERE</p>
-                      <a href="https://github.com/loganjaymes/TRIFECTA">Repository</a>
+                    <div className="flex flex-row gap-4 w-full justify-between items-center">
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button className="hover:cursor-pointer">Tech Stack & Info</Button>
+                        </DialogTrigger>
+                        <DialogContent className="border-blue-950 bg-zinc-950 border-2 text-zinc-300">
+                          <DialogHeader>
+                            <DialogTitle>EnviroPact</DialogTitle>
+                            <DialogDescription className="text-zinc-400">
+                              Created for SASEHacks2024 in less than three days, EnviroPact is a social media web app focused on community engagement through enviromental cleanups.
+                              Users can create, join, and search events in their area, RVSPing with their Google Account, all to find others and work towards the common goal of improving our environment.
+                              <br />
+                              &nbsp;
+                              <br />
+                              Created with
+                              <div className="flex flex-row p-1 gap-2">
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image className="invert" src="/techicons/react.svg" width={100} height={100} alt="react" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/tailwind.svg" width={100} height={100} alt="tailwind css" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/ant.svg" width={100} height={100} alt="ant design" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/framer.svg" width={100} height={100} alt="framer motion" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/firebase.png" width={100} height={100} alt="firebase and firestore" />
+                                </Button>
+                              </div>
+                            </DialogDescription>
+                          </DialogHeader>
+                        </DialogContent>
+                      </Dialog>
+                      <a className="text-blue-300 hover:underline duration-100 hover:shadow-lg shadow-blue-950" href="https://github.com/Kavipatel0/SASEHacks2024">Repository</a>
                     </div>
                   </CardFooter>
                 </Card>
@@ -52,13 +100,41 @@ export default function projects() {
                     <CardTitle className="text-3xl">TRIFECTA</CardTitle>
                   </CardHeader>
                   <CardContent className="flex flex-col flex-grow gap-4 w-full items-center">
-                      <Image className="rounded-md" src="/proj/trifect.png" width={600} height={200} alt="TRIFECTA image displaying help info"/>
-                      BLURB FIXME
+                      <Image className="rounded-md" src="/proj/trif.png" width={600} height={200} alt="TRIFECTA image displaying help info"/>
+                      {/* BLURB FIXME */}
                   </CardContent>
                   <CardFooter>
-                    <div className="flex flex-row gap-4 w-full justify-between">
-                      <p>TECH SHIT HERE</p>
-                      <a href="https://github.com/loganjaymes/TRIFECTA">Repository</a>
+                    <div className="flex flex-row gap-4 w-full justify-between items-center">
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button className="hover:cursor-pointer">Tech Stack & Info</Button>
+                        </DialogTrigger>
+                        <DialogContent className="border-blue-950 bg-zinc-950 border-2 text-zinc-300">
+                          <DialogHeader>
+                            <DialogTitle>TRIFECTA</DialogTitle>
+                            <DialogDescription className="text-zinc-400">
+                              Trifecta is a Discord bot that automatically tracks any scores sent into a text channel from the NYT Wordle, Mini-Crossword, and Connections. 
+                              Each user's score is logged into their respective game's daily leaderboard in ascending order.
+                              <br />
+                              &nbsp;
+                              <br />
+                              Created with
+                              <div className="flex flex-row p-1 gap-2">
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/python.png" width={100} height={100} alt="python" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/discordpy.png" width={100} height={100} alt="discord.py" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image className="invert" src="/techicons/aws.png" width={100} height={100} alt="amazon web services" />
+                                </Button>
+                              </div>
+                            </DialogDescription>
+                          </DialogHeader>
+                        </DialogContent>
+                      </Dialog>
+                      <a className="text-blue-300 hover:underline duration-100 hover:shadow-lg shadow-blue-950" href="https://github.com/loganjaymes/TRIFECTA">Repository</a>
                     </div>
                   </CardFooter>
                 </Card>
@@ -73,16 +149,48 @@ export default function projects() {
               <div className="flex max-w-lg min-h-full">
                 <Card className="bg-slate-gray-950 text-zinc-300 overflow-y-auto">
                   <CardHeader>
-                    <CardTitle>6 Degrees</CardTitle>
+                    <CardTitle className="text-3xl">Six Degrees</CardTitle>
                   </CardHeader>
                   <CardContent className="flex flex-col flex-grow gap-4 w-full items-center">
                     <Image className="rounded-md" src="/proj/sixdeg.png" width={600} height={200} alt="A screenshot displaying some connections between the movies Dune and Ponyo"/>
-                    BLURB FIXME
+                    {/* BLURB FIXME */}
                   </CardContent>
                   <CardFooter>
-                    <div className="flex flex-row gap-4 w-full justify-between">
-                      <p>TECH SHIT HERE</p>
-                      <a href="https://github.com/AlexanderWangY/movie-linker">Repository</a>
+                    <div className="flex flex-row gap-4 w-full justify-between items-center">
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button className="hover:cursor-pointer">Tech Stack & Info</Button>
+                        </DialogTrigger>
+                        <DialogContent className="border-blue-950 bg-zinc-950 border-2 text-zinc-300">
+                          <DialogHeader>
+                            <DialogTitle>Six Degrees</DialogTitle>
+                            <DialogDescription className="text-zinc-400">
+                              This project aims to emulate the idea that no two humans currently alive are separated by more than 6 degrees. 
+                              In the same way, we hypothesize that no two films made after the 2000s have more than 6 degrees of connections between shared actors. 
+                              Meaning, any film within our dataset should be able to link to another based on actors within 6 nodes.
+                              <br />
+                              &nbsp;
+                              <br />
+                              Created with
+                              <div className="flex flex-row p-1 gap-2">
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image className="invert" src="/techicons/svelte.png" width={100} height={100} alt="svelte" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/tailwind.svg" width={100} height={100} alt="tailwind css" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/skeletonui.png" width={100} height={100} alt="skeleton ui" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/rust.svg" width={100} height={100} alt="rust" />
+                                </Button>
+                              </div>
+                            </DialogDescription>
+                          </DialogHeader>
+                        </DialogContent>
+                      </Dialog>
+                      <a className="text-blue-300 hover:underline duration-100 hover:shadow-lg shadow-blue-950" href="https://github.com/AlexanderWangY/movie-linker">Repository</a>
                     </div>
                   </CardFooter>
                 </Card>
@@ -94,17 +202,49 @@ export default function projects() {
               <div className="flex max-w-lg min-h-full">
                 <Card className="bg-slate-gray-950 text-zinc-300 overflow-y-auto">
                   <CardHeader>
-                    <CardTitle>SEES Signup</CardTitle>
+                    <CardTitle className="text-3xl">SEES Sign-Up</CardTitle>
                   </CardHeader>
                   <CardContent className="flex flex-col flex-grow gap-4 w-full items-center">
                     <Image className="rounded-md" src="/proj/seeswip.png" width={600} height={200} alt="A WIP image showing a low poly blender rig"/>
-                    BLURB FIXME
+                    {/* BLURB FIXME */}
                   </CardContent>
                   <CardFooter>
-                    <div className="flex flex-row gap-4 w-full justify-between">
-                      <p>TECH SHIT HERE</p>
-                      <p>Planned Proposal</p>
-                      <a href="https://github.com/Kavipatel0/SASEHacks2024">Repository</a>
+                    <div className="flex flex-row gap-4 w-full justify-between items-center">
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button className="hover:cursor-pointer">Tech Stack & Info</Button>
+                        </DialogTrigger>
+                        <DialogContent className="border-blue-950 bg-zinc-950 border-2 text-zinc-300">
+                          <DialogHeader>
+                            <DialogTitle>SEES Sign-Up</DialogTitle>
+                            <DialogDescription className="text-zinc-400">
+                              A work-in-progress web app composed of things I already know, things I want to learn, and things I think are cool. 
+                              With that being said, it is a project based on one of my interests, the Persona series, that combines all of the above.
+                              As of right now, I am currently in the pre-production stage of the web app, however, I plan to use it as a learning experience more than anything,
+                              as it is forcing me to learn a handful of different coding and art mediums.
+                              <br />
+                              &nbsp;
+                              <br />
+                              Created with (subject to change)
+                              <div className="flex flex-row p-1 gap-2">
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image className="invert" src="/techicons/threejs.png" width={100} height={100} alt="three js" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/tailwind.svg" width={100} height={100} alt="tailwind css" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/blender.png" width={100} height={100} alt="blender" />
+                                </Button>
+                                <Button size="icon" className="rounded-md overflow-auto">
+                                  <Image src="/techicons/firebase.png" width={100} height={100} alt="firebase and firestore" />
+                                </Button>
+                              </div>
+                            </DialogDescription>
+                          </DialogHeader>
+                        </DialogContent>
+                      </Dialog>
+                      <a className="text-blue-300 hover:underline duration-100 hover:shadow-lg shadow-blue-950" href="https://github.com/Kavipatel0/SASEHacks2024">Repository</a>
                     </div>
                   </CardFooter>
                 </Card>
